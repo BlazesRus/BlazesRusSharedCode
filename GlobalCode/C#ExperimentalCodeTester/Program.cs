5f8511358c19d8b86852e5c80b1445649e07b0d2
BlazesRus <BlazesRus@yahoo.com>
2017-02-19 16:56:15 -0500

    some missing parts added

---
+++ GlobalCode/C#ExperimentalCodeTester/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+	using CSharpGlobalCode.GlobalCode_ExperimentalCode;
+	class Program
+    {
+        static void Main(string[] args)
+        {
+			////Console.WriteLine("Extraction Test=" + CSharpGlobalCode.GlobalCode_VariableConversionFunctions.VariableConversionFunctions.ExtractDecimalHalf(0.5));
+			//SuperDec_ExtraDec32_19Decimal Value01 = (SuperDec_ExtraDec32_19Decimal)1.5;
+			//Console.WriteLine("CurrentValue=" + Value01);
+			//SmallDec Value02 = (SmallDec) Value01;
+			//Console.WriteLine("CurrentValue=" + Value02);
+			//Value02 += Value01;
+			//Console.WriteLine("CurrentValue=" + Value02);
+			SmallDec Value01 = (SmallDec)1.5;
+			SuperDec_Int32_9Decimal Value02 = (SuperDec_Int32_9Decimal)Value01;
+			Console.WriteLine("CurrentValue=" + (string) Value02);
+			//SuperDec_ExtraDec32_19Decimal Value01 = BaseValue;
+			//Value01 *= 2;
+			//Console.WriteLine("CurrentValue=" + Value01);
+			//Value01 = 10.5;
+			//Value01 *= 10.25;
+			//Console.WriteLine("CurrentValue=" + Value01);
+			System.Threading.Thread.Sleep(4000);
+        }
+    }
+}
---
+++ GlobalCode/GlobalCode_StringFunctions/GlobalCode_MiscFunctions/MiscFunctions.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSharpGlobalCode.GlobalCode_MiscFunctions
+{
+	//class VariableScanning
+	//{
+	//	bool VariableTypeScanned = false;
+	//	bool VariableNameScanned = false;
+	//	string VariableTypeName = "";
+	//	string VariableName = "";
+	//	// true = LineComments; false = Multi-line comments
+	//	bool LineCommentType = false;
+	//	bool ScanningComments = false;
+	//	string CommentStringBuffer;
+	//	dynamic TabLevel = 0;
+	//	dynamic StringLength;
+	//	dynamic TempInt;
+	//	char StringChar;
+	//	bool DisplayCommentsToConsole = true;
+	//	bool SaveCommentData = true;
+	//	string CommentDataString = "";
+	//	StringVectorList CommentDataBuffer;
+	//	//CommentData is sent here when sending variable data
+	//	StringVectorList LinkedCommentData;
+	//	bool CreateGetFunctions = false;
+	//	StringVectorList VariableNameList;
+	//	StringVectorList VariableTypeList;
+	//	string ReconstructVariableAssignment(string Name, string Type);
+	//	void CreateFileWithVariableData(string FileName);
+	//	void SendVariableAssignmentInfo();
+	//	void ScanData(string LineString);
+	//};
+
+	//class MiscFunctions
+ //   {
+	//	////************************************
+	//	//// Method:    FindWrappedText
+	//	//// FullName:  MiscFunctions::FindWrappedText
+	//	//// Access:    public static 
+	//	//// Returns:   bool
+	//	//// Qualifier:
+	//	//// Parameter: string LineString
+	//	//// Parameter: string SearchedString
+	//	////************************************
+	//	//static bool FindWrappedText(string LineString, string SearchedString);
+	//	////************************************
+	//	//// Method:    TextNotFound
+	//	//// FullName:  MiscFunctions::TextNotFound
+	//	//// Access:    public static 
+	//	//// Returns:   bool
+	//	//// Qualifier:
+	//	//// Parameter: string LineString
+	//	//// Parameter: string SearchedString
+	//	////************************************
+	//	//static bool TextNotFound(string LineString, string SearchedString);
+	//	////************************************
+	//	//// Method:    RearrangedVariableDataFromFile
+	//	//// FullName:  MiscFunctions::RearrangedVariableDataFromFile
+	//	//// Access:    public static 
+	//	//// Returns:   bool
+	//	//// Qualifier:
+	//	//// Parameter: string FileName
+	//	//// Parameter: string OutputFileName
+	//	////************************************
+	//	//static bool RearrangedVariableDataFromFile(string FileName, string OutputFileName);
+	//	////************************************
+	//	//// Method:    ConvertFileToString
+	//	//// FullName:  MiscFunctions::ConvertFileToString
+	//	//// Access:    public static 
+	//	//// Returns:   string
+	//	//// Qualifier:
+	//	//// Parameter: string FileName
+	//	////************************************
+	//	//static string ConvertFileToString(string FileName);
+	//	////************************************
+	//	//// Method:    ConvertStringToFile
+	//	//// FullName:  MiscFunctions::ConvertStringToFile
+	//	//// Access:    public static 
+	//	//// Returns:   void
+	//	//// Qualifier:
+	//	//// Parameter: string FileName
+	//	//// Parameter: string TargetString
+	//	////************************************
+	//	//static void ConvertStringToFile(string FileName, string TargetString);
+	//	////************************************
+	//	//// Method:    IsRecordedHavokClassType
+	//	//// FullName:  MiscFunctions::IsRecordedHavokClassType
+	//	//// Access:    public static 
+	//	//// Returns:   bool
+	//	//// Qualifier:
+	//	//// Parameter: string HavokClass
+	//	////************************************
+	//	//static bool IsRecordedHavokClassType(string HavokClass);
+	//	////************************************
+	//	//// Method:    ExtractFirstTagContents
+	//	//// FullName:  MiscFunctions::ExtractFirstTagContents
+	//	//// Access:    public static 
+	//	//// Returns:   string
+	//	//// Qualifier:
+	//	//// Parameter: string LineString
+	//	////************************************
+	//	//static string ExtractFirstTagContents(string LineString);
+	//	////Removes extra spacing,tabs, and new lines between characters
+	//	//static string RemoveExcessSpacing(string LineString);
+	//	////Near Text match of text that ignores extra spacing/tabs separating text
+	//	//static bool FindTextWithNearMatch(string LineString, string SearchedString);
+	//	////************************************
+	//	//// Method:    RetrieveTopLevelObjectFromString
+	//	//// FullName:  MiscFunctions::RetrieveTopLevelObjectFromString
+	//	//// Access:    public 
+	//	//// Returns:   string
+	//	//// Qualifier:
+	//	//// Parameter: string LineString
+	//	////************************************
+	//	//string RetrieveTopLevelObjectFromString(string LineString);
+	//}
+}
