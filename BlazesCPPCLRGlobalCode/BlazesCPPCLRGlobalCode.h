63ec43b9e9a2d88d12c3590cdc17a2a0aab100b8
BlazesRus <BlazesRus@yahoo.com>
2018-02-09 17:58:17 -0500

    Created Project directories

---
+++ BlazesCPPCLRGlobalCode/BlazesCPPCLRGlobalCode.h
@@ -0,0 +1,14 @@
+// BlazesCPPCLRGlobalCode.h
+
+#pragma once
+
+using namespace System;
+
+namespace BlazesGlobalCode 
+{
+
+	public ref class Class1
+	{
+		// TODO: Add your methods for this class here.
+	};
+}
