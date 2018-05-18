27403ec941dc9672d27f92b4a6005f1211c48eb1
BlazesRus <BlazesRus@yahoo.com>
2018-05-18 19:10:11 -0400

    Added more operation tests for Code tester

--- CSharpGlobalCodeTester/Program.cs
+++ CSharpGlobalCodeTester/Program.cs
@@ -14,15 +14,24 @@ namespace CSharpGlobalCodeTester
             SmallDec Self = (SmallDec)"1.1";
             SmallDec Value = (SmallDec)"1.1452";
             Self *= Value;//Should equal 1.25972
-            Console.WriteLine("Test01 Answer:" + Self);
+            Console.WriteLine("Test01 Answer:" + Self.ToOptimalString());
             Self = (SmallDec)"0.1";
             Value = (SmallDec)"0.1";
             Self /= Value;//Should equal 1
-            Console.WriteLine("Test02 Answer:" + Self);
+            Console.WriteLine("Test02 Answer:" + Self.ToOptimalString());
             Self = (SmallDec)"1.0";
             Value = (SmallDec)"0.1";
             Self *= Value;//Should equal 0.1
-            Console.WriteLine("Test03 Answer:" + Self);
+            Console.WriteLine("Test03 Answer:" + Self.ToOptimalString());
+            Self = (SmallDec)"0.9";
+            Value = (SmallDec)"0.9";
+            Self *= Value;//Should equal 0.81
+            Console.WriteLine("0.9 x 0.9=" + Self.ToOptimalString());
+            Self = (SmallDec)"2.5";
+            Value = (SmallDec)"0.5";
+            Self *= Value;//Should equal 1.25
+            Console.WriteLine("2.5 x 0.5=" + Self.ToOptimalString());
+            Console.ReadLine();
         }
     }
 }
