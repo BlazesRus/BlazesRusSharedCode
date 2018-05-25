1210938ce07fd315f56f1a5d33960d9fc2af35e0
BlazesRus <BlazesRus@yahoo.com>
2018-05-24 21:52:43 -0400

    Started separation of Fractional part of Pow Function

--- BlazesCSharpGlobalCode/GlobalCode_ExperimentalCode/SmallDec_MathBasedFunctions.cs
+++ BlazesCSharpGlobalCode/GlobalCode_ExperimentalCode/SmallDec_MathBasedFunctions.cs
@@ -9,11 +9,7 @@ using System;
 //CSharpGlobalCode.GlobalCode_ExperimentalCode.SmallDec
 namespace CSharpGlobalCode.GlobalCode_ExperimentalCode
 {
-    using System.Collections.Generic;
     using System.ComponentModel;
-    using System.Globalization;
-    using System.Windows;
-    using static GlobalCode_VariableConversionFunctions.VariableConversionFunctions;
 
     // Represent +- 65535.999999999(Can only represent +- 65535.9999 if SmallDec_ReducedSize or SmallDec_UseLegacyStorage set) with 100% consistency of accuracy
     //(Aka SuperDec_Int16_9Decimal Or SuperDec_Int16_4Decimal)
@@ -30,6 +26,7 @@ namespace CSharpGlobalCode.GlobalCode_ExperimentalCode
     SmallDec : IFormattable, INotifyPropertyChanged
     {
 #if (!SmallDec_ReducedSize && !SmallDec_UseLegacyStorage)//Only enable for current implementation of SmallDec
+
         /// <summary>
         /// Removes decimal place info on self
         /// </summary>
@@ -86,12 +83,13 @@ namespace CSharpGlobalCode.GlobalCode_ExperimentalCode
         }
 
         /// <summary>
-        /// Perform square root on this instance. 
+        /// Perform square root on this instance.
         /// Info on Related Formulas:
         /// https://jacksondunstan.com/articles/1217
         /// https://en.wikipedia.org/wiki/Nth_root_algorithm
         /// </summary>
 #if !BlazesGlobalCode_PostCompileBasedSqrt
+
         public void Sqrt()
         {//Unfinished
             if (DecimalStatus == 0)
@@ -152,11 +150,16 @@ namespace CSharpGlobalCode.GlobalCode_ExperimentalCode
                 AssignFromVal(Temp);
             }
         }
+
 #else
         public void Sqrt()
         {
         }
 #endif
+
+        //public static SmallDec NthRoot(SmallDec self, int Value)
+        //{
+        //}
 
         /// <summary>
         /// Gets the square root of the specified value.
@@ -218,15 +221,20 @@ namespace CSharpGlobalCode.GlobalCode_ExperimentalCode
         }
 
 #if (!BlazesGlobalCode_ImplicitConversionFrom)//Gets confused since it tries auto converting to SmallDec inside parameter first if Explicitly converts
-        public static SmallDec Pow(double self, double Value) { return SmallDec.Pow((SmallDec)self, (SmallDec)Value); }
+
+        public static SmallDec Pow(double self, double Value)
+        {
+            return SmallDec.Pow((SmallDec)self, (SmallDec)Value);
+        }
 
         ///// <summary>
-        ///// 
+        /////
         ///// </summary>
         ///// <param name="self"></param>
         ///// <param name="Value"></param>
         ///// <returns></returns>
         public static SmallDec Pow(SmallDec self, double Value) => SmallDec.Pow(self, (SmallDec)Value);
+
 #endif
 
         ///// <summary>
@@ -246,7 +254,6 @@ namespace CSharpGlobalCode.GlobalCode_ExperimentalCode
         //{
         //}
 
-
         /// <summary>
         /// Approximate version of Math.Pow(double self, double Value) based on https://www.geeksforgeeks.org/write-an-iterative-olog-y-function-for-powx-y/
         /// and https://stackoverflow.com/questions/3606734/calculate-fractional-exponent-in-for-loop-without-power-function
@@ -256,33 +263,53 @@ namespace CSharpGlobalCode.GlobalCode_ExperimentalCode
         /// <returns></returns>
         public static SmallDec Pow(SmallDec self, SmallDec Value)
         {
-            if(Value.DecimalStatus==0)
+            if (Value.DecimalStatus == 0)
             {
                 return SmallDec.Pow(self, Value.IntValue);
             }
-            else if(Value.DecimalStatus==NegativeWholeNumber)
+            else if (Value.DecimalStatus == NegativeWholeNumber)
             {
-                return SmallDec.Pow(self, Value.IntValue*-1);
+                return SmallDec.Pow(self, Value.IntValue * -1);
             }
-            else if(Value.DecimalStatus>0)//Positive Non-Whole Number Value
+            else if (Value.DecimalStatus > 0)//Positive Non-Whole Number Value
             {
-                ////Separate Value into Fractional
-                ////long ValueAsIntRep = Value.IntValue * DecimalOverflow + Value.DecimalStatus;
-                //long PartOne;
-                //long PartTwo;
-                //if(Value.IntValue<10)
-                //{
-
-                //}
-                //else if(Value.IntValue<100)
-                //{
-
-                //}
-                //else if(Value.IntValue<1000)
-                //{
-
-                //}
-                //else if(Value.IntValue)
+                //Separate Value into Fractional
+                //long ValueAsIntRep = Value.IntValue * DecimalOverflow + Value.DecimalStatus;
+                //Value = (Value.DecimalStatus * DecimalOverflow)/ 0.DecimalStatus * DecimalOverflow
+                long PartOne = (long)Value.IntValue * DecimalOverflow;
+                long PartTwo = Value.DecimalStatus;
+                bool TryToDecreasePlacement = true;
+                bool KeepAttempting = true;
+                for (int Attempts = 0; Attempts < 14 && KeepAttempting; ++Attempts)//Reduce Fractional Size
+                {
+                    if (TryToDecreasePlacement)//Try to remove excess decimal places first
+                    {
+                        if (PartTwo % 100000 == 0 && PartOne % 100000 == 0) { PartTwo /= 100000; PartOne /= 100000; }
+                        else if (PartTwo % 100 == 0 && PartOne % 100 == 0) { PartTwo /= 100; PartOne /= 100; }
+                        else if (PartTwo % 10 == 0 && PartOne % 10 == 0) { PartTwo /= 10; PartOne /= 10; }
+                        else
+                        {
+                            TryToDecreasePlacement = false;
+                        }
+                    }
+                    else
+                    {
+                        if (PartTwo % 7 == 0 && PartOne % 7 == 0) { PartTwo /= 7; PartOne /= 7; }
+                        else if (PartTwo % 5 == 0 && PartOne % 5 == 0) { PartTwo /= 5; PartOne /= 5; }
+                        else if (PartTwo % 3 == 0 && PartOne % 3 == 0) { PartTwo /= 3; PartOne /= 3; }
+                        else if (PartTwo % 2 == 0 && PartOne % 2 == 0) { PartTwo /= 2; PartOne /= 2; }
+                        else if (PartTwo % 11 == 0 && PartOne % 11 == 0) { PartTwo /= 11; PartOne /= 11; }
+                        else if (PartTwo % 13 == 0 && PartOne % 13 == 0) { PartTwo /= 13; PartOne /= 13; }
+                        else if (PartTwo % 17 == 0 && PartOne % 17 == 0) { PartTwo /= 17; PartOne /= 17; }
+                        else if (PartTwo % 23 == 0 && PartOne % 23 == 0) { PartTwo /= 23; PartOne /= 23; }
+                        else if (PartTwo % 29 == 0 && PartOne % 29 == 0) { PartTwo /= 29; PartOne /= 29; }
+                        else if (PartTwo % 31 == 0 && PartOne % 31 == 0) { PartTwo /= 31; PartOne /= 31; }
+                        else
+                        {
+                            KeepAttempting = false;
+                        }
+                    }
+                }
                 //Working placeholder code for now
                 double SelfAsDecimal = (double)self;
                 double ValueAsDecimal = (double)Value;
@@ -313,4 +340,4 @@ namespace CSharpGlobalCode.GlobalCode_ExperimentalCode
     }
 
 #endif
-    }
+}
