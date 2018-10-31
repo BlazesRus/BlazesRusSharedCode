dfe46d3092325d7158f65b6c0cc62c2ac5e1d04a
BlazesRus <BlazesRus@yahoo.com>
2018-10-30 21:22:49 -0400

    manually adding BEGIN_MULTITEMPLATE_MESSAGE_MAP usage without defining to try getting closer to finding where errors coming from

--- GlobalCode/PagedMultiviewTreeControl/BasicMultiviewTemplate.cpp
+++ GlobalCode/PagedMultiviewTreeControl/BasicMultiviewTemplate.cpp
@@ -15,36 +15,50 @@
 static char THIS_FILE[] = __FILE__;
 #endif
 
-#if !defined(BEGIN_MULTITEMPLATE_MESSAGE_MAP)
-#define BEGIN_MULTITEMPLATE_MESSAGE_MAP(theClass, type_name, typename02, typename03, typename04, baseClass)			\
-	PTM_WARNING_DISABLE														\
-	template < typename type_name, typename typename02, typename typename03, typename typename04 >											\
-	const AFX_MSGMAP* theClass< type_name, typename02, typename03, typename04>::GetMessageMap() const			\
-	{ return GetThisMessageMap(); }										\
-	template < typename type_name, typename typename02, typename typename03, typename typename04 >											\
-	const AFX_MSGMAP* PASCAL theClass< type_name, typename02, typename03, typename04 >::GetThisMessageMap()		\
-	{																		\
-		typedef theClass< type_name, typename02, typename03, typename04 > ThisClass;							\
-		typedef baseClass TheBaseClass;										\
-		__pragma(warning(push))												\
-		__pragma(warning(disable: 4640)) /* message maps can only be called by single threaded message pump */ \
-		static const AFX_MSGMAP_ENTRY _messageEntries[] =					\
-		{
-#define END_MULTITEMPLATE_MAP() \
-			{0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 } \
-		}; \
-	__pragma(warning(pop))	\
-	static const AFX_MSGMAP messageMap = \
-	{ &TheBaseClass::GetThisMessageMap, &_messageEntries[0] }; \
-	return &messageMap; \
-	}								  \
-	PTM_WARNING_RESTORE
-#endif
+////Based on AFX BEGIN_TEMPLATE_MESSAGE_MAP
+//#if !defined(BEGIN_MULTITEMPLATE_MESSAGE_MAP)
+//#define BEGIN_MULTITEMPLATE_MESSAGE_MAP(theClass, type_name, typename02, typename03, typename04, baseClass)			\
+//	PTM_WARNING_DISABLE														\
+//	template < typename type_name, typename typename02, typename typename03, typename typename04 >											\
+//	const AFX_MSGMAP* theClass< type_name, typename02, typename03, typename04>::GetMessageMap() const			\
+//	{ return GetThisMessageMap(); }										\
+//	template < typename type_name, typename typename02, typename typename03, typename typename04 >											\
+//	const AFX_MSGMAP* PASCAL theClass< type_name, typename02, typename03, typename04 >::GetThisMessageMap()		\
+//	{																		\
+//		typedef theClass< type_name, typename02, typename03, typename04 > ThisClass;							\
+//		typedef baseClass TheBaseClass;										\
+//		__pragma(warning(push))												\
+//		__pragma(warning(disable: 4640)) /* message maps can only be called by single threaded message pump */ \
+//		static const AFX_MSGMAP_ENTRY _messageEntries[] =					\
+//		{
+//#define END_MULTITEMPLATE_MAP() \
+//			{0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 } \
+//		}; \
+//	__pragma(warning(pop))	\
+//	static const AFX_MSGMAP messageMap = \
+//	{ &TheBaseClass::GetThisMessageMap, &_messageEntries[0] }; \
+//	return &messageMap; \
+//	}								  \
+//	PTM_WARNING_RESTORE
+//#endif
 
 /////////////////////////////////////////////////////////////////////////////
 // BasicMultiviewTemplate
 
-BEGIN_MULTITEMPLATE_MESSAGE_MAP(BasicMultiviewTemplate, ViewType01, ViewType02, WindowType, FrameWindowType, CWinAppEx)
+//BEGIN_MULTITEMPLATE_MESSAGE_MAP(BasicMultiviewTemplate, ViewType01, ViewType02, WindowType, FrameWindowType, CWinAppEx)
+PTM_WARNING_DISABLE														\
+template<typename ViewType01, typename ViewType02, typename WindowType, typename FrameWindowType>									\
+const AFX_MSGMAP* BasicMultiviewTemplate<ViewType01, ViewType02, WindowType, FrameWindowType>::GetMessageMap() const			\
+{ return GetThisMessageMap(); }										\
+template<typename ViewType01, typename ViewType02, typename WindowType, typename FrameWindowType>										\
+const AFX_MSGMAP* PASCAL BasicMultiviewTemplate<ViewType01, ViewType02, WindowType, FrameWindowType>::GetThisMessageMap()		\
+{																		\
+typedef theClass< type_name, typename02, typename03, typename04 > ThisClass;							\
+typedef baseClass TheBaseClass;										\
+__pragma(warning(push))												\
+__pragma(warning(disable: 4640)) /* message maps can only be called by single threaded message pump */ \
+static const AFX_MSGMAP_ENTRY _messageEntries[] = \
+{
 	//{{AFX_MSG_MAP(BasicMultiviewTemplate)
 	ON_COMMAND(ID_APP_ABOUT, OnAppAbout)
 	ON_COMMAND(ID_VIEW_OTHERVIEW, OnViewOtherview)
@@ -55,7 +69,17 @@ BEGIN_MULTITEMPLATE_MESSAGE_MAP(BasicMultiviewTemplate, ViewType01, ViewType02, 
 	ON_COMMAND(ID_FILE_OPEN, CWinApp::OnFileOpen)
 	// Standard print setup command
 	ON_COMMAND(ID_FILE_PRINT_SETUP, CWinApp::OnFilePrintSetup)
-END_MULTITEMPLATE_MAP()
+//END_MULTITEMPLATE_MAP()
+	{
+		0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0
+	} \
+}; \
+__pragma(warning(pop))	\
+static const AFX_MSGMAP messageMap = \
+		{ &CWinAppEx::GetThisMessageMap, &_messageEntries[0] }; \
+			return &messageMap; \
+}								  \
+PTM_WARNING_RESTORE
 
 /////////////////////////////////////////////////////////////////////////////
 // BasicMultiviewTemplate construction
