3b6532c1020736bc8408217b39e507719f33a1ed
BlazesRus <BlazesRus@yahoo.com>
2018-11-14 01:14:24 -0500

    manually using macro for debugging purposes

--- GlobalCode/PagedMultiviewTreeControl/TreePage.h
+++ GlobalCode/PagedMultiviewTreeControl/TreePage.h
@@ -21,8 +21,27 @@ public:
 class TreePage : public CustomTreeView<TreePageNode>
 {
 public:
-	BEGIN_AltMESSAGE_MAP()
-	END_AltMESSAGE_MAP_Base01(CustomTreeView, TreePageNode)
+	//BEGIN_AltMESSAGE_MAP()
+	//END_AltMESSAGE_MAP_Base01(CustomTreeView, TreePageNode)
+protected:
+	static const AFX_MSGMAP* PASCAL GetThisMessageMap()
+	{
+		__pragma(warning(push))
+		__pragma(warning(disable: 4640))
+		static const AFX_MSGMAP_ENTRY _messageEntries[] =
+		{
+			{ 0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 }
+		};
+		__pragma(warning(pop))
+		static const AFX_MSGMAP messageMap =
+		{ &CustomTreeView<TreePageNode>::GetThisMessageMap, &_messageEntries[0] };
+		return &messageMap;
+	}
+public:
+	virtual const AFX_MSGMAP* GetMessageMap() const
+	{
+		return GetThisMessageMap();
+	}
 };
 
 #endif
