54365c54d722058e63866c50b8647ecce0c85e06
BlazesRus <BlazesRus@yahoo.com>
2018-11-18 17:16:42 -0500

    moved some defines into WP_APPDefines

--- GlobalCode/PagedMultiviewTreeControl/CustomTreeNode.h
+++ GlobalCode/PagedMultiviewTreeControl/CustomTreeNode.h
@@ -2,25 +2,7 @@
 #ifndef CustomTreeNode_Header
 #define CustomTreeNode_Header
 
-#define		CM_INSERTCHILD				WM_APP + 10000
-#define		CM_INSERTSIBLING			WM_APP + 10001
-#define		CM_DELETENODE				WM_APP + 10002
-#define		CM_MODIFYNODETEXT			WM_APP + 10003
-#define		CM_CHANGENODECOLOR			WM_APP + 10004
-#define		CM_TOGGLECONNECTINGLINES	WM_APP + 10010
-#define		CM_SETCONNECTINGLINESCOLOR	WM_APP + 10011
-#define		CM_SETFONT					WM_APP + 10020
-#define		CM_SETDEFAULTCOLOR			WM_APP + 10021
-#define		CM_SETBACKGROUNDBITMAP		WM_APP + 10022
-#define		CM_TOGGLEMENUSOUND			WM_APP + 10030
-
-#if !defined(STRCPY)
-#if defined( _UNICODE )
-#define STRCPY(x,y)				wcscpy(x,y)
-#else
-#define STRCPY(x,y)				strcpy(x,y)
-#endif
-#endif
+#include "WP_APPDefines.h"
 
 #include "MultiviewPrecompile.h"
 //#include "CustomTreeControl.h"
---
+++ GlobalCode/PagedMultiviewTreeControl/WP_APPDefines.h
@@ -0,0 +1,24 @@
+#if !defined(WP_APPDefines_IncludeGuard)
+#define WP_APPDefines_IncludeGuard
+
+#define		CM_INSERTCHILD				WM_APP + 10000
+#define		CM_INSERTSIBLING			WM_APP + 10001
+#define		CM_DELETENODE				WM_APP + 10002
+#define		CM_MODIFYNODETEXT			WM_APP + 10003
+#define		CM_CHANGENODECOLOR			WM_APP + 10004
+#define		CM_TOGGLECONNECTINGLINES	WM_APP + 10010
+#define		CM_SETCONNECTINGLINESCOLOR	WM_APP + 10011
+#define		CM_SETFONT					WM_APP + 10020
+#define		CM_SETDEFAULTCOLOR			WM_APP + 10021
+#define		CM_SETBACKGROUNDBITMAP		WM_APP + 10022
+#define		CM_TOGGLEMENUSOUND			WM_APP + 10030
+
+#if !defined(STRCPY)
+#if defined( _UNICODE )
+#define STRCPY(x,y)				wcscpy(x,y)
+#else
+#define STRCPY(x,y)				strcpy(x,y)
+#endif
+#endif
+
+#endif
