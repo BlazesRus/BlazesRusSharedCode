06ea2265c9f1771c4bbd66c9104a5dc6851888fc
BlazesRus (James) <BlazesRus@yahoo.com>
2021-10-10 19:16:03 -0400

    remaned class_name parameter to className

--- SharedCode/OtherFunctions/MFCMacrosV3.h
+++ SharedCode/OtherFunctions/MFCMacrosV3.h
@@ -6,30 +6,30 @@
 #pragma once
 
 /*
-class class_name: public baseClass
+class className: public baseClass
 {
-    class_name(){}
-    ~class_name(){}
+    className(){}
+    ~className(){}
     //And other class stuff
-    MFC_RuntimeExtPart01(class_name, baseClass)
+    MFC_RuntimeExtPart01(className, baseClass)
     //Any message map messages here
-    MFC_RuntimeExtPart02(class_name)
+    MFC_RuntimeExtPart02(className)
 };
 
-MFC_RuntimeImplimentation(class_name)
+MFC_RuntimeImplimentation(className)
 */
-#define MFC_RuntimeExtPart01(class_name, baseClass)\
+#define MFC_RuntimeExtPart01(className, baseClass)\
 private:\
     typedef baseClass TheBaseClass;\
-    typedef class_name ThisClass;\
+    typedef className ThisClass;\
 protected:\
     static CRuntimeClass* PASCAL _GetBaseClass() { return TheBaseClass::GetThisClass(); } \
 public:\
-    static const std::string class_nameStr;\
-    static LPCSTR ClassName() { return class_nameStr.c_str(); }\
-    static const CRuntimeClass class##class_name;\
-    static CRuntimeClass* PASCAL GetThisClass() { return _RUNTIME_CLASS(class_name); }\
-    virtual CRuntimeClass* GetRuntimeClass() const { return _RUNTIME_CLASS(class_name); }\
+    static const std::string classNameStr;\
+    static LPCSTR ClassName() { return classNameStr.c_str(); }\
+    static const CRuntimeClass class##className;\
+    static CRuntimeClass* PASCAL GetThisClass() { return _RUNTIME_CLASS(className); }\
+    virtual CRuntimeClass* GetRuntimeClass() const { return _RUNTIME_CLASS(className); }\
 protected:\
     static const AFX_MSGMAP* PASCAL GetThisMessageMap()\
     {\
@@ -38,7 +38,7 @@ protected:\
         static const AFX_MSGMAP_ENTRY _messageEntries[] =\
         {
 
-#define MFC_RuntimeExtPart02(class_name)\
+#define MFC_RuntimeExtPart02(className)\
             { 0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 }\
         };\
         __pragma(warning(pop))\
@@ -48,7 +48,7 @@ protected:\
     }\
     static std::string ClassString()\
     {\
-        return "class_name";\
+        return "className";\
     }\
 public:\
     virtual const AFX_MSGMAP* GetMessageMap() const\
@@ -56,30 +56,34 @@ public:\
         return GetThisMessageMap();\
     }
 
+#define MFC_RuntimeImplimentation(className)\
+const std::string className::classNameStr = className::classNameStr;\
+const CRuntimeClass className::class##className = { "className", sizeof(className), 0xFFFF, NULL,&className::_GetBaseClass, NULL, NULL };
+
 /*
-class class_name: public baseClass<baseArg01>
+class className: public baseClass<baseArg01>
 {
-    class_name(){}
-    ~class_name(){}
+    className(){}
+    ~className(){}
     //And other class stuff
-    MFC_RuntimeExtPart01Base01(class_name, baseClass, baseArg01)
+    MFC_RuntimeExtPart01Base01(className, baseClass, baseArg01)
     //Any message map messages here
-    MFC_RuntimeExtPart02(class_name)
+    MFC_RuntimeExtPart02(className)
 };
 
-MFC_RuntimeImplimentation(class_name)
+MFC_RuntimeImplimentation(className)
 */
-#define MFC_RuntimeExtPart01Base01(class_name, baseClass, baseArg01)\
+#define MFC_RuntimeExtPart01Base01(className, baseClass, baseArg01)\
 private:\
     typedef baseClass<baseArg01> TheBaseClass;\
-    typedef class_name ThisClass;\
+    typedef className ThisClass;\
 protected:\
     static CRuntimeClass* PASCAL _GetBaseClass() { return TheBaseClass::GetThisClass(); } \
 public:\
-    static LPCSTR ClassName() { return class_name##Str.c_str(); }\
-    static const CRuntimeClass class##class_name;\
-    static CRuntimeClass* PASCAL GetThisClass() { return _RUNTIME_CLASS(class_name); }\
-    virtual CRuntimeClass* GetRuntimeClass() const { return _RUNTIME_CLASS(class_name); }\
+    static LPCSTR ClassName() { return className##Str.c_str(); }\
+    static const CRuntimeClass class##className;\
+    static CRuntimeClass* PASCAL GetThisClass() { return _RUNTIME_CLASS(className); }\
+    virtual CRuntimeClass* GetRuntimeClass() const { return _RUNTIME_CLASS(className); }\
 protected:\
     static const AFX_MSGMAP* PASCAL GetThisMessageMap()\
     {\
@@ -89,30 +93,30 @@ protected:\
         {
 
 /*
-class class_name: public baseClass<baseArg01, baseArg02>
+class className: public baseClass<baseArg01, baseArg02>
 {
-    class_name(){}
-    ~class_name(){}
+    className(){}
+    ~className(){}
     //And other class stuff
     
-    MFC_RuntimeExtPart01Base02(class_name, baseClass, baseArg01, baseArg02)
+    MFC_RuntimeExtPart01Base02(className, baseClass, baseArg01, baseArg02)
     //Any message map messages here
-    MFC_RuntimeExtPart02(class_name)
+    MFC_RuntimeExtPart02(className)
 };
 
-MFC_RuntimeImplimentation(class_name)
+MFC_RuntimeImplimentation(className)
 */
-#define MFC_RuntimeExtPart01Base02(class_name, baseClass, baseArg01, baseArg02)\
+#define MFC_RuntimeExtPart01Base02(className, baseClass, baseArg01, baseArg02)\
 private:\
     typedef baseClass<baseArg01, baseArg02> TheBaseClass;\
-    typedef class_name ThisClass;\
+    typedef className ThisClass;\
 protected:\
     static CRuntimeClass* PASCAL _GetBaseClass() { return TheBaseClass::GetThisClass(); } \
 public:\
-    static LPCSTR ClassName() { return class_nameStr.c_str(); }\
-    static const CRuntimeClass class##class_name;\
-    static CRuntimeClass* PASCAL GetThisClass() { return _RUNTIME_CLASS(class_name); }\
-    virtual CRuntimeClass* GetRuntimeClass() const { return _RUNTIME_CLASS(class_name); }\
+    static LPCSTR ClassName() { return classNameStr.c_str(); }\
+    static const CRuntimeClass class##className;\
+    static CRuntimeClass* PASCAL GetThisClass() { return _RUNTIME_CLASS(className); }\
+    virtual CRuntimeClass* GetRuntimeClass() const { return _RUNTIME_CLASS(className); }\
 protected:\
     static const AFX_MSGMAP* PASCAL GetThisMessageMap()\
     {\
@@ -121,44 +125,40 @@ protected:\
         static const AFX_MSGMAP_ENTRY _messageEntries[] =\
         {
 
-#define MFC_RuntimeImplimentation(class_name)\
-const std::string class_name::class_name##Str = class_name::class_nameStr;\
-const CRuntimeClass class_name::class##class_name = { "class_name", sizeof(class_name), 0xFFFF, NULL,&class_name::_GetBaseClass, NULL, NULL };
+#define MFC_RuntimeImplimentationWith01Args(className,arg01)\
+const std::string className::classNameStr = className::className##_##arg01Str;\
+const CRuntimeClass className::class##className_##arg01 = { "className<arg01>", sizeof(className<arg01>), 0xFFFF, NULL,&className::_GetBaseClass, NULL, NULL };
 
-#define MFC_RuntimeImplimentationWith01Args(class_name,arg01)\
-const std::string class_name::class_nameStr = class_name::class_name##_##arg01Str;\
-const CRuntimeClass class_name::class##class_name_##arg01 = { "class_name<arg01>", sizeof(class_name<arg01>), 0xFFFF, NULL,&class_name::_GetBaseClass, NULL, NULL };
-
-#define MFC_RuntimeImplimentationWith02Args(class_name,arg01,arg02)\
-const std::string class_name::class_name_##arg01_##arg02Str = class_name::class_name##_##arg01##_##arg02Str;\
-const CRuntimeClass class_name::class##class_name_##arg01_##arg02 = { "class_name<arg01,arg02>", sizeof(class_name<arg01,arg02>), 0xFFFF, NULL,&class_name::_GetBaseClass, NULL, NULL };
+#define MFC_RuntimeImplimentationWith02Args(className,arg01,arg02)\
+const std::string className::className_##arg01_##arg02Str = className::className##_##arg01##_##arg02Str;\
+const CRuntimeClass className::class##className_##arg01_##arg02 = { "className<arg01,arg02>", sizeof(className<arg01,arg02>), 0xFFFF, NULL,&className::_GetBaseClass, NULL, NULL };
 
 /*
 template<typename arg01>
-class class_name: public baseClass
+class className: public baseClass
 {
-    class_name(){}
-    ~class_name(){}
+    className(){}
+    ~className(){}
     //And other class stuff
-    MFC_RuntimeExtPart01With01Args(class_name, arg01, baseClass)
+    MFC_RuntimeExtPart01With01Args(className, arg01, baseClass)
     //Any message map messages here
-    MFC_RuntimeExtPart02With01Args(class_name, arg01)
+    MFC_RuntimeExtPart02With01Args(className, arg01)
 };
 
-MFC_RuntimeImplimentationWith01Args(class_name, arg01)
+MFC_RuntimeImplimentationWith01Args(className, arg01)
 */
-#define MFC_RuntimeExtPart01With01Args(class_name, arg01, baseClass)\
+#define MFC_RuntimeExtPart01With01Args(className, arg01, baseClass)\
 private:\
     typedef baseClass TheBaseClass;\
-    typedef class_name<arg01> ThisClass;\
+    typedef className<arg01> ThisClass;\
 protected:\
     static CRuntimeClass* PASCAL _GetBaseClass() { return TheBaseClass::GetThisClass(); } \
 public:\
-    static const std::string class_name##_##arg01##Str;\
-    static LPCSTR ClassName() { return class_name##_##arg01##Str.c_str(); }\
-    static const CRuntimeClass class##class_name##_##arg01;\
-    static CRuntimeClass* PASCAL GetThisClass() { return ((CRuntimeClass*)(&class_name<arg01>::class##class_name##_##arg01)) }\
-    virtual CRuntimeClass* GetRuntimeClass() const { return ((CRuntimeClass*)(&class_name<arg01>::class##class_name##_##arg01)) }\
+    static const std::string className##_##arg01##Str;\
+    static LPCSTR ClassName() { return className##_##arg01##Str.c_str(); }\
+    static const CRuntimeClass class##className##_##arg01;\
+    static CRuntimeClass* PASCAL GetThisClass() { return ((CRuntimeClass*)(&className<arg01>::class##className##_##arg01)) }\
+    virtual CRuntimeClass* GetRuntimeClass() const { return ((CRuntimeClass*)(&className<arg01>::class##className##_##arg01)) }\
 protected:\
     static const AFX_MSGMAP* PASCAL GetThisMessageMap()\
     {\
@@ -167,7 +167,7 @@ protected:\
         static const AFX_MSGMAP_ENTRY _messageEntries[] =\
         {
 
-#define MFC_RuntimeExtPart02With01Args(class_name, arg01)\
+#define MFC_RuntimeExtPart02With01Args(className, arg01)\
             { 0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 }\
         };\
         __pragma(warning(pop))\
@@ -177,7 +177,7 @@ protected:\
     }\
     static std::string ClassString()\
     {\
-        std::string Combined = "class_name<"; \
+        std::string Combined = "className<"; \
         Combined += typeid(arg01).name(); \
         Combined += ">"; \
         return Combined; \
@@ -190,30 +190,30 @@ public:\
     
 /*
 template<typename arg01>
-class class_name: public baseClass
+class className: public baseClass
 {
-    class_name(){}
-    ~class_name(){}
+    className(){}
+    ~className(){}
     //And other class stuff
-    MFC_RuntimeExtPart01With02Args(class_name, arg01, arg02, baseClass)
+    MFC_RuntimeExtPart01With02Args(className, arg01, arg02, baseClass)
     //Any message map messages here
-    MFC_RuntimeExtPart02With02Args(class_name, arg01, arg02)
+    MFC_RuntimeExtPart02With02Args(className, arg01, arg02)
 };
 
-MFC_RuntimeImplimentationWith02Args(class_name, arg01, arg02)
+MFC_RuntimeImplimentationWith02Args(className, arg01, arg02)
 */
-#define MFC_RuntimeExtPart01With02Args(class_name, arg01, arg02, baseClass)\
+#define MFC_RuntimeExtPart01With02Args(className, arg01, arg02, baseClass)\
 private:\
     typedef baseClass TheBaseClass;\
-    typedef class_name<arg01, arg02> ThisClass;\
+    typedef className<arg01, arg02> ThisClass;\
 protected:\
     static CRuntimeClass* PASCAL _GetBaseClass() { return TheBaseClass::GetThisClass(); } \
 public:\
-    static const std::string class_name##_##arg01##_##arg02##Str;\
-    static LPCSTR ClassName() { return class_name##_##arg01##_##arg02##Str.c_str(); }\
-    static const CRuntimeClass class##class_name##_##arg01##_##arg02;\
-    static CRuntimeClass* PASCAL GetThisClass() { return ((CRuntimeClass*)(&class_name<arg01>::class##class_name##_##arg01##_##arg02)) }\
-    virtual CRuntimeClass* GetRuntimeClass() const { return ((CRuntimeClass*)(&class_name<arg01>::class##class_name##_##arg01##_##arg02)) }\
+    static const std::string className##_##arg01##_##arg02##Str;\
+    static LPCSTR ClassName() { return className##_##arg01##_##arg02##Str.c_str(); }\
+    static const CRuntimeClass class##className##_##arg01##_##arg02;\
+    static CRuntimeClass* PASCAL GetThisClass() { return ((CRuntimeClass*)(&className<arg01>::class##className##_##arg01##_##arg02)) }\
+    virtual CRuntimeClass* GetRuntimeClass() const { return ((CRuntimeClass*)(&className<arg01>::class##className##_##arg01##_##arg02)) }\
 protected:\
     static const AFX_MSGMAP* PASCAL GetThisMessageMap()\
     {\
@@ -222,7 +222,7 @@ protected:\
         static const AFX_MSGMAP_ENTRY _messageEntries[] =\
         {
 
-#define MFC_RuntimeExtPart02With02Args(class_name, arg01, arg02)\
+#define MFC_RuntimeExtPart02With02Args(className, arg01, arg02)\
             { 0, 0, 0, 0, AfxSig_end, (AFX_PMSG)0 }\
         };\
         __pragma(warning(pop))\
@@ -232,7 +232,7 @@ protected:\
     }\
     static std::string ClassString()\
     {\
-        std::string Combined = "class_name<"; \
+        std::string Combined = "className<"; \
         Combined += typeid(arg01).name(); \
         Combined += ", "; \
         Combined += typeid(arg02).name(); \
